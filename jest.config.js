ed118cab7f942dc64733f2ab09d1a5608b816703
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-05-18 19:53:18 -0700

    build: edit jest config, install testing lib

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: 'node',
+	testEnvironment: 'jest-environment-jsdom',
 	moduleNameMapper: {
 		'^@components/(.*)$': '<rootDir>/src/components/$1',
 		'^@constants/(.*)$': '<rootDir>/src/constants/$1',
+		'^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
 		'^@utils/(.*)$': '<rootDir>/src/utils/$1',
 	},
 };
