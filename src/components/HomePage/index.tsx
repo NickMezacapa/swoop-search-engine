9e8c0f1efbd3fdd0b414df2869f5ea1bc5e3197a
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-05-25 20:35:11 -0700

    style: edit search filter container placement

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -27,7 +27,7 @@ const HomePage = ({ variant }: HomePageProps) => {
                 <SearchFormPrimary />
                 <HomeSuggestions />
                 <div className='hidden md:block w-[160%]'>
-                    <h1 className='font-semibold leading-[1.08349] tracking-[-0.003em] text-[#6e6e73] text-2xl mt-[3.75rem] mb-[1rem]'>Filter Results</h1>
+                    <h1 className='font-semibold leading-[1.08349] tracking-[-0.003em] text-[#6e6e73] text-2xl mt-[3.5rem] mb-[1rem]'>Filter Results</h1>
                     <HomeSearchFilters />
                 </div>
             </div>
