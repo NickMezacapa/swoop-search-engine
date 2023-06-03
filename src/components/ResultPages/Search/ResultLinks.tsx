f2e773c25d81f4451065448969d9bb345f2feefe
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-06-02 23:56:56 -0700

    styles: add shimmer effect to skeleton loader

--- src/components/ResultPages/Search/ResultLinks.tsx
+++ src/components/ResultPages/Search/ResultLinks.tsx
@@ -8,9 +8,25 @@ interface ResultLinksProps {
 const ResultLinks = ({ query }: ResultLinksProps) => {
     const { loading, error, data } = useGetSearchResults(query, false);
     if (loading) {
-        return <div className='text-5xl text-[#1d1d1f]'>Loading...</div>
+        const arr = Array(10).fill('')
+        return (
+            <div className='SkeletonLoaderEntrance flex flex-col w-1/2 opacity-0 h-full max-w-[800px] transition ease'>
+                {arr.map((_, i) => {
+                    return (
+                        <div 
+                            key={i}
+                            className='bg-[#EAE8ED] w-full relative overflow-hidden transition shadow-md mb-8 text-[#1d1d1f] dark:bg-[#39393cb1] dark:text-[#eae8ed] border border-[hsla(0,0%,51%,0.2)] flex flex-col space-y-2 rounded-lg p-2'>
+                            <div className='SkeletonShimmer bg-gradient-to-r from-transparent to-[#cecccfc3] dark:to-[#484849b1] via-transparent" rounded-lg py-2 bg-[#cecccf] dark:bg-[#444446b1] w-1/2' />
+                            <div className='SkeletonShimmer bg-gradient-to-r from-transparent to-[#cecccfc3] dark:to-[#484849b1] via-transparent" rounded-lg py-4 bg-[#6f6f7157] dark:bg-[#585859b1] w-1/3' />
+                            <div className='SkeletonShimmer bg-gradient-to-r from-transparent to-[#cecccfc3] dark:to-[#484849b1] via-transparent" rounded-lg py-5 bg-[#a2a1a341] dark:bg-[#444446b1] w-3/4' />
+                        </div>
+                    );
+                })}
+            </div>
+        );
     } 
     if (!data) {
+        console.log('There seemed to be an error making the request...', error);
         return <p>No results found.</p>
     }
   return (
@@ -21,17 +37,17 @@ const ResultLinks = ({ query }: ResultLinksProps) => {
                 <motion.div 
                     key={result.url}
                     initial={{
-                        x: '-60%',
+                        y: '50%',
                         opacity: 0,
                     }}
                     animate={{
-                        x: 0,
+                        y: 0,
                         opacity: 1,
                     }}
                     transition={{
                         duration: 0.25,
-                        ease: 'linear',
-                        delay: index * 0.15
+                        ease: 'easeOut',
+                        delay: index * 0.08
                     }}
                     className='bg-[#EAE8ED] cursor-pointer overflow-hidden transition hover:scale-[0.99] shadow-md mb-8 text-[#1d1d1f] dark:bg-[#39393cb1] dark:text-[#eae8ed] flex flex-col space-y-2 rounded-lg p-2'>
                     <div className='group'>
