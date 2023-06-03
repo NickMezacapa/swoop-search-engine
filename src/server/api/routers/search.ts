d3a772fa81f95c53dfb66f65e14da54f90d5d150
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-06-03 00:00:45 -0700

    bug: remove broken import

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -2,26 +2,26 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@server/api/trpc";
 
-import { fetchSearchResults } from '@utils/helpers/fetchSearchResults';
-import { fetchImageResults } from '@utils/helpers/fetchImageResults';
+/* import { fetchSearchResults } from '@utils/helpers/fetchSearchResults';
+import { fetchImageResults } from '@utils/helpers/fetchImageResults'; */
 
 const searchRouter = createTRPCRouter({
-    swoopSearch: publicProcedure
-        .input(z.object({ query: z.string() }))
-        .query(async ({ input }) => {
-            const searchResults = await fetchSearchResults(input.query)
-            return {
-                items: searchResults,
-            };
-        }),
-    imageSearch: publicProcedure
-        .input(z.object({ query: z.string() }))
-        .query(async ({ input }) => {
-            const imageResults = await fetchImageResults(input.query)
-            return {
-                items: imageResults,
-            }
-        }),
+    /*     swoopSearch: publicProcedure
+            .input(z.object({ query: z.string() }))
+            .query(async ({ input }) => {
+                const searchResults = await fetchSearchResults(input.query)
+                return {
+                    items: searchResults,
+                };
+            }),
+        imageSearch: publicProcedure
+            .input(z.object({ query: z.string() }))
+            .query(async ({ input }) => {
+                const imageResults = await fetchImageResults(input.query)
+                return {
+                    items: imageResults,
+                }
+            }), */
 });
 
 export { searchRouter };
